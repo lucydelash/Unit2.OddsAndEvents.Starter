6753b57aaa19f0a1c27b56cb2fed69951484251e
lucydelash <lucyrudd614@gmail.com>
2024-03-20 15:21:01 -0400

    added pseudocode & linked html to js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// TODO: this file! :)
+// make the number bank an empty list
+// users will submits integers using the "add number" button
+// numbers are added to the number bank section
+// the submitted numbers are displayed in the number bank section
+// sort 1 button: takes the first integer from the number bank and sorts it to either the odd or even section
+// sort all: takes all the numbers in the numberBank and sorts them to either the odd or even section
